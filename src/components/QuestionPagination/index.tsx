8d924ecab6c5f32621f0c41bae108f7ef382e31b
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2024-08-25 20:09:26 -0300

    feat: adjust in layout to mobile

--- src/components/QuestionPagination/index.tsx
+++ src/components/QuestionPagination/index.tsx
@@ -5,6 +5,6 @@ export default function QuestionPagination() {
   const totalQuestions = getTotalQuestionsInCategory(selectCategory);
   const currentQuestion = currentQuestionIndex + 1;
   return(
-    <h1 className='text-2xl font-bold text-gray-800 text-center mb-4'>Pergunta { currentQuestion } de {totalQuestions} - {selectCategory}</h1>
+    <h1 className='text-2xl font-bold text-gray-800 text-center mb-4'>{ currentQuestion } / {totalQuestions} - {selectCategory}</h1>
   )
 }
--- src/components/Score/index.tsx
+++ src/components/Score/index.tsx
@@ -4,8 +4,8 @@ export default function QuizScore() {
   const { getScoreByCorrectAwnsers } = useQuiz();
   const score = getScoreByCorrectAwnsers();
   return (
-    <div className="p-2 w-full bg-gray-100 border-2 border-gray-300 rounded-lg shadow-md text-center">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">Pontuação</h1>
+    <div className="p-2 w-full bg-gray-100 border-2 border-gray-300 rounded-lg shadow-md text-center flex justify-between align-middle">
+      <h1 className="text-2xl font-bold text-gray-800">Score: </h1>
       <p className="text-4xl font-extrabold text-blue-500">{score}</p>
     </div>
   );
