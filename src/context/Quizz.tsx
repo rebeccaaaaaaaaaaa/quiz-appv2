18d8b2bec9dc978f4ad0e13f2f0aaf8bdbc60c88
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2024-08-25 07:56:18 -0300

    feat: add data.json in public folder

--- src/context/Quizz.tsx
+++ src/context/Quizz.tsx
@@ -1,26 +1,77 @@
-import { createContext, useEffect } from 'react';
+import { createContext, useEffect, useState, ReactNode } from 'react';
+
+interface Question {
+  category: string;
+  id: number;
+  question: string;
+  options: string[];
+  correctAnswer: string;
+}
+
+interface QuizData {
+  category: string;
+  questions: Question[];
+}
 
 interface QuizzContextData {
-  message: string;
+  quizData: QuizData | null;
+  getQuestions: () => Question[] | null;
+  selectCategory: string;
+  setSelectCategory: (category: string) => void;
+  getCategories: () => string[];
 }
 
-interface  QuizzContextProps {
-  children: React.ReactNode;
+interface QuizzContextProps {
+  children: ReactNode;
 }
 
-export const QuizContext = createContext<QuizzContextData>(
-  {} as QuizzContextData
-);
+export const QuizContext = createContext<QuizzContextData>({} as QuizzContextData);
 
-export function  QuizzProvider({ children }: QuizzContextProps){
-  const message = 'Hello from QuizzProvider';
+export function QuizzProvider({ children }: QuizzContextProps) {
+  const [quizData, setQuizData] = useState<QuizData | null>(null);
+  const [selectCategory, setSelectCategory] = useState<string>('');
+
+  function getCategories(): string[] {
+    if (quizData) {
+      // Extrair categorias únicas
+      const categories = quizData.questions
+        .map(question => question.category)
+        .filter((value, index, self) => self.indexOf(value) === index); // Remove duplicatas
+      return categories;
+    }
+    return [];
+  }
+  
+
   useEffect(() => {
-    console.log('QuizzProvider mounted');
+    const fetchQuizData = async () => {
+      try {
+        const response = await fetch('/data.json');
+        const data = await response.json();
+        setQuizData(data.quiz);
+      } catch (error) {
+        console.error('Failed to load quiz data:', error);
+      }
+    };
+    fetchQuizData();
   }, []);
 
+  const getQuestions = (): Question[] | null => {
+    if (quizData) {
+      return quizData.questions;
+    }
+    return null;
+  };
+
   return (
-    <QuizContext.Provider value={{ message }}>
+    <QuizContext.Provider value={{ 
+      quizData,
+      getQuestions,
+      selectCategory,
+      setSelectCategory,
+      getCategories
+     }}>
       {children}
     </QuizContext.Provider>
   );
-};
+}
